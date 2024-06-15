a91000c748002cc213506f8e2190c6aa3712d0f2
ANTHONY LIMA E SILVA <72108893+Anthony-a-l-s@users.noreply.github.com>
2024-06-14 23:46:10 -0300

    add pagina home e detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,67 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-function Filme(){
-    return(
-        <div>
-            <h1>Mem vindo a DEtalhes do Filme</h1>
-        </div>
-    )
+import api from "../../services/api";
+import './filme-info.css'
+
+
+function Filme() {
+
+    const { id } = useParams();
+
+    const [filme, setFilme] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: '1833fa2cf920f4f2c968289ec056514b',
+                    language: 'pt-BR',
+                }
+            }
+
+            )
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    console.log('filme não encontrado')
+                })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log('componente desmontado')
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <div>
+                <h1 className="flime-info ...">Carregando detalhes ...</h1>
+            </div>
+        )
+    } else {
+        return (
+            <div className="flime-info">
+                <h1>{filme.title}</h1>
+                <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+                <h3>Sinopse</h3>
+                <span>{filme.overview}</span>
+                <strong>Avaliação: {filme.vote_average} /10</strong>
+                <div className="area-buttons">
+                    <button>Salvar</button>
+                    <button>
+                        <a href='#'>Trailer</a>
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Filme;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,53 @@
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css'
 
-function Home(){
-    return(
-        <div>
-            <h1>Mem vindo a Home</h1>
-        </div>
-    )
+function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function loadingFilmes() {
+            const response = await api.get('/movie/now_playing', {
+                params: {
+                    api_key: '1833fa2cf920f4f2c968289ec056514b',
+                    language: 'pt-BR',
+                    page: 1,
+                }
+            })
+            setFilmes(response.data.results.slice(0, 10));
+            setLoading(false);
+        }
+
+        loadingFilmes();
+
+    }, [])
+    if (loading) {
+        return (
+            <div className='loading'>
+                <h2>Carregnado filmes ...</h2>
+            </div>
+        )
+    } else {
+        return (
+            <div className='coniainer'>
+                <div className='lista-filmes'>
+                    {filmes.map((filme) => {
+                        return (
+                            <article key={filme.id}>
+                                <strong>{filme.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Home;
